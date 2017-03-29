efedef8efd6ac128d579a9c28f79a8062ed7a7ed
ErandyR <rerandy15@gmail.com>
2017-03-29 16:31:53 +0000

    se crean los titulos de listas

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,8 @@
-alert("creando trello!!");
+
+function agregarLista(){
+  var texto = document.getElementById("ingresarLista").value;
+  var tituloLista = document.createElement("h3");
+  var textoTitulo = document.createTextNode(texto);
+  tituloLista.appendChild(textoTitulo);
+  document.getElementById("nuevaLista").appendChild(tituloLista);
+}
