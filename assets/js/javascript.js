ea9b36a243428f81e77107c4dd2a860583a70ac0
ErandyR <rerandy15@gmail.com>
2017-03-29 19:33:44 +0000

    creando botones borrar

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,19 +5,54 @@ function agregarLista(){
   var textoTitulo = document.createTextNode(texto);
   tituloLista.appendChild(textoTitulo);
   document.getElementById("nuevaLista").appendChild(tituloLista);
+
 }
 function crearPendiente(){
+  var h5 = document.createElement("h4");
+  var agregarPendiente = document.createTextNode("Agregar pendiente");
+  h5.appendChild(agregarPendiente);
+  document.getElementById("nuevaLista").appendChild(h5);
+
   var nuevoInput = document.createElement("input");
   var nuevoBoton = document.createElement("button");
   nuevoInput.type = "texto";
   nuevoInput.id = "inputPendiente";
   nuevoBoton.type = "button";
   nuevoBoton.id = "botonPendiente";
-  nuevoBoton.onclick = function listaPendiente(){
-                        alert("funciona!!");
-                      }
-  var textoBoton = document.createTextNode("Agregar Pendiente");
+  var textoBoton = document.createTextNode("Crear Pendiente");
   nuevoBoton.appendChild(textoBoton);
+
+  nuevoBoton.onclick = function listaDePendientes(){
+                          var seccion = document.createElement("section");
+                          var ul = document.createElement("ul");
+                          ul.id = "lista";
+                          var li = document.createElement("li");
+                          var pendiente = document.getElementById("inputPendiente").value;
+                          var listaPendiente = document.createTextNode(pendiente);
+                          li.appendChild(listaPendiente);
+                          ul.appendChild(li);
+                          seccion.appendChild(ul);
+                          document.getElementById("nuevaLista").appendChild(seccion);
+                          pendiente.innerHTML = "";
+                          var btnBorrarPendiente = document.createElement("button");
+                          btnBorrarPendiente.type = "button";
+                          btnBorrarPendiente.id = "borrarPendiente";
+                          var textoBorrarPendiente = document.createTextNode("Borrar");
+                          btnBorrarPendiente.appendChild(textoBorrarPendiente);
+                          document.getElementById("lista").appendChild(btnBorrarPendiente);
+                      }
   document.getElementById("nuevaLista").appendChild(nuevoInput);
   document.getElementById("nuevaLista").appendChild(nuevoBoton);
+  botonBorrarLista();
+}
+function botonBorrarLista(){
+  var btnBorrar = document.createElement("button");
+  btnBorrar.type = "button";
+  btnBorrar.id = "borrar";
+  var textoBotonBorrar = document.createTextNode("Borrar");
+  btnBorrar.appendChild(textoBotonBorrar);
+  document.getElementById("nuevaLista").appendChild(btnBorrar);
+  btnBorrar.onclick = function borrar(){
+                        alert("borremos la lista!");
+                      }
 }
