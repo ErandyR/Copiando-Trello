beb52207a9d6cef878ded8ab18b0b1a76c89065e
ErandyR <rerandy15@gmail.com>
2017-03-29 17:00:47 +0000

    se crea input y boton de pendientes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,3 +6,17 @@ function agregarLista(){
   tituloLista.appendChild(textoTitulo);
   document.getElementById("nuevaLista").appendChild(tituloLista);
 }
+function crearPendiente(){
+  var nuevoInput = document.createElement("input");
+  var nuevoBoton = document.createElement("button");
+  nuevoInput.type = "texto";
+  nuevoInput.id = "inputPendiente";
+  nuevoBoton.type = "button";
+  nuevoBoton.id = "botonPendiente";
+  nuevoBoton.onclick = "listaPendiente();";
+  document.getElementById("nuevaLista").appendChild(nuevoInput);
+  document.getElementById("nuevaLista").appendChild(nuevoBoton);
+}
+function listaPendiente(){
+  alert("funciona!!");
+}
